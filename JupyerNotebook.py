f5b81871190ed8b10938ca873c0f94c67bae08a4
juannavarrosoto <114248153+juannavarrosoto@users.noreply.github.com>
2022-09-26 11:27:51 -0500

    Create JupyerNotebook.py

---
+++ JupyerNotebook.py
@@ -0,0 +1,14 @@
+My Jupyter Notebook on IBM Watson Studio
+**Juan Sebastian Navarro** 
+    
+    Project Engineer 
+*I am interested in data science because is the best way to optimize any proccess at any level.*
+
+*I would like to star my career as data scientist because data are the most valuable thing in any company in order to known and improve the results.*
+<H3>In cell 5 we are going to find the result after multiply 5 and 6<H3>
+5*6
+Data science
+Python
+Pandas
+Jupyter
+"href=https://www.datacamp.com/tutorial/markdown-in-jupyter-notebook#internal-and-external-link" Link to Jupyter Manual
